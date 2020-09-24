c0ad00ce39bc9faad4cbd721de5d1cea8fca27c3
Sunghyun Park <cmos00@nate.com>
2020-09-25 03:29:33 +0900

    초기 커밋.

---
+++ SmallGOF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmallGOF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
